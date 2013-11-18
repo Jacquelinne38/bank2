48d05df07da0cdb3f515712d3a4d4784e408b65b
gudaENSIMAG <pizzix@hotmail.com>
2013-11-18 22:27:35 +0100

    adding

---
+++ src/main/java/com/ensimag/bank2/App.java
@@ -0,0 +1,13 @@
+package com.ensimag.bank2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
